33d5f2d59ff973a68f5b31760e8cf786fe385130
Dusan Krstic <krstic.duci@gmail.com>
2023-05-29 17:16:23 +0200

    concurent scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import bodyParser from "body-parser";
-import cors from "cors";
+import cors, { CorsRequest } from "cors";
 import express from "express";
 import { readFileSync } from "fs";
 import http from "http";
@@ -11,50 +11,40 @@ import http from "http";
 import { db } from "./db/index.js";
 import { resolvers } from "./resolvers/index.js";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
 
-// Required logic for integrating with Express
 const app = express();
 app.disable("x-powered-by");
 
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
 const httpServer = http.createServer(app);
 
-// Same ApolloServer initialization as before, plus the drain plugin
-// for our httpServer.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
-// Ensure we wait for our server to start
-await server.start();
 
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
-app.use(
-  "/graphql",
-  cors<cors.CorsRequest>(),
-  // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
-  bodyParser.json({ limit: "50mb" }),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
-  expressMiddleware(server, {
-    // TODO: check this
-    context: async ({ req }) => ({ token: req.headers.token })
+db.connect()
+  .then(async () => {
+    await server.start();
+
+    app.use(
+      "/graphql",
+      cors<CorsRequest>(),
+      // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+      bodyParser.json({ limit: "50mb" }),
+      // expressMiddleware accepts the same arguments:
+      // an Apollo Server instance and optional configuration options
+      expressMiddleware(server, {
+        // TODO: check this
+        context: async ({ req }) => ({ token: req.headers.token })
+      })
+    );
+
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port: 4000 }, resolve)
+    );
   })
-);
-
-// Modified server startup
-await new Promise<void>((resolve) =>
-  httpServer.listen({ port: 4000 }, resolve)
-);
-console.log(`Server ready at http://localhost:4000/graphql`);
-
-// Database connection
-db.connect();
+  .finally(() => {
+    console.log(`Server ready at http://localhost:4000/graphql`);
+  });
